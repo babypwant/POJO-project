7a9a6664b55ee43a559ad59bde7d58d8ad5af4b4
gary <baby_plant@icloud.com>
2021-03-16 15:56:09 -0700

    solved 7

--- problems/07-break-down-object.js
+++ problems/07-break-down-object.js
@@ -15,7 +15,10 @@ breakDownObj(object2); // => [ 'location', 'borough', 'NY', 'Brooklyn' ]
 ***********************************************************************/
 
 function breakDownObj(obj) {
-  // your code here
+  let keys = Object.keys(obj);
+  let values = Object.values(obj);
+  return [...keys, ...values];
+
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
--- problems/08-value-pair.js
+++ problems/08-value-pair.js
@@ -9,9 +9,17 @@ let object2 = {name: 'Two', location: 'SF'};
 valuePair(object1, object2, 'location'); // => [ 'NY', 'SF' ]
 valuePair(object1, object2, 'name'); // => [ 'One', 'Two' ]
 ***********************************************************************/
-
+// create a new array that contains the values of key
+//we learned that we can use Objects.values(obj)
 function valuePair(obj1, obj2, key) {
-  // your code here
+  let newArr = []
+  if (Object.values(obj1) === key) {
+    newArr.push(obj1[key])
+  }
+  if (Object.values(obj2) === key) {
+    newArr.push(obj2[key])
+  }
+  console.log(newArr);
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
